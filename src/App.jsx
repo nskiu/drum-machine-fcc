ba59b1b464ddac36d366cb3d7f2c436aa1427c5d
nskiu <nyelsku@gmail.com>
2023-06-10 12:58:25 +0800

    Improve drumpad render algo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import Controls from "./components/Controls";
 import DrumPad from "./components/DrumPad";
+import sounds from "./assets/sounds.js";
+import { useState } from "react";
+
+const text = sounds;
 
 const App = () => {
+  const [beats, setBeats] = useState(text.set1);
   const handleClick = () => {
     console.log("clicked");
   };
-
+  console.log(beats);
   return (
     <div id="drum-machine">
       <div id="logo">Logo</div>
-      <DrumPad handleClick={handleClick} />
+      <DrumPad handleClick={handleClick} beats={beats} />
       <Controls />
     </div>
   );
---
+++ src/assets/sounds.js
@@ -0,0 +1,42 @@
+const sounds = {
+  set1: {
+    Q: {
+      name: "Heater 1",
+      source: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    },
+    W: {
+      name: "Heater 2",
+      source: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    },
+    E: {
+      name: "Heater 3",
+      source: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    },
+    A: {
+      name: "Heater 4",
+      source: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    },
+    S: {
+      name: "Clap",
+      source: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    },
+    D: {
+      name: "Open-HH",
+      source: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    },
+    Z: {
+      name: "Kick-n'-Hat",
+      source: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    },
+    X: {
+      name: "Kick",
+      source: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    },
+    C: {
+      name: "Closed-HH",
+      source: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    },
+  },
+};
+
+export default sounds;
